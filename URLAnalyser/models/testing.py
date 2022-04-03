1326040a07330912969ddc0a67fe927bd0e551eb
Edward Gorman <ejgorman@gmail.com>
2022-04-03 19:20:24 +0100

    #5: Add sklearn scoring metrics

---
+++ URLAnalyser/models/testing.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import recall_score
+
+
+def generate_predictions(model, isKeras, x_test):
+    predictions = model.predict(x_test)
+    if isKeras:
+        predictions = (predictions > 0.5).astype(np.int)
+    return predictions
+
+def calculate_metrics(predictions, true_values):
+    return {
+        'accuracy': round(accuracy_score(true_values, predictions), 3),
+        'precision': round(precision_score(true_values, predictions), 3),
+        'recall': round(recall_score(true_values, predictions), 3),
+        'f1': round(f1_score(true_values, predictions), 3),
+    }
